ed41c9bb4c6ee95706a860a386ae6428a01cc470
Vyacheslav Morov <v.morov@corp.mail.ru>
2018-06-19 17:23:14 +0300

    Template

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rest-server-py',
+      version='1.0',
+      description='Python REST server',
+      author='Vyacheslav Morov',
+      author_email='vyacheslav.morov@gmail.com',
+      url='',
+      packages=['rest_server'],
+      )
